6892986862ede571ed07672ef27c4d025383cee7
EvelineGomes <gomes.evepg@gmail.com>
2023-04-14 18:20:36 -0400

    initialize init method using window

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -1,15 +1,9 @@
-// defining a namespace to avoid future problems regarding variable names
 const App = {
-  // we'll add selected items as well as initialization methods
-  // 1st: use a prefix, such as $ and put all of our selected HTML elements
-  // so this $ will be a property of the this namespace
   $: {
     menu: document.querySelector(".menu"),
-    menuItems: document.querySelector(".items"), // can't use menu.querySelector bcz now it isn't global anymore, and can't use App.$.menu either because App hasn't been initialized yet
+    menuItems: document.querySelector(".items"),
   },
 
-  // the init() method is where we'll add the event listeners to our app
-  // ~1h41min more syntax on how to add this init() method
   init() {
     App.$.menu.addEventListener("click", (event) => {
       App.$.menuItems.classList.toggle("hidden");
@@ -17,10 +11,5 @@ const App = {
   },
 };
 
-// this won't work just as menu.etc... because it's now App.$.menu
-// same for menuItems, which is now App.$.menuItems
-// However, like this we're letting things in the global scope; so we gotta add an init() method whitin the App namespace
-
-// menu.addEventListener("click", (event) => {
-//   menuItems.classList.toggle("hidden");
-// });
+// To initialize init() we gotta use the window and the object and use event listener as the load with the callback function as the init method:
+window.addEventListener("load", () => App.init());
