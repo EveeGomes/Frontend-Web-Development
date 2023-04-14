6891f254a7152297b03c1e1eae7a53ba9501f230
EvelineGomes <gomes.evepg@gmail.com>
2023-04-14 18:17:51 -0400

    add namespace with properties and init method

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -1,9 +1,26 @@
-// use the querySelector and add a class name so it'll select any element in the html whithin that class
-const menu = document.querySelector(".menu");
-const menuItems = menu.querySelector(".items");
+// defining a namespace to avoid future problems regarding variable names
+const App = {
+  // we'll add selected items as well as initialization methods
+  // 1st: use a prefix, such as $ and put all of our selected HTML elements
+  // so this $ will be a property of the this namespace
+  $: {
+    menu: document.querySelector(".menu"),
+    menuItems: document.querySelector(".items"), // can't use menu.querySelector bcz now it isn't global anymore, and can't use App.$.menu either because App hasn't been initialized yet
+  },
 
-// now, once we see that the button on the menu has been clicked, we'll add some functionality
-// check video @1h:30m till ~33min for explanation on how to come up with that solution
-menu.addEventListener("click", (event) => {
-  menuItems.classList.toggle("hidden");
-});
+  // the init() method is where we'll add the event listeners to our app
+  // ~1h41min more syntax on how to add this init() method
+  init() {
+    App.$.menu.addEventListener("click", (event) => {
+      App.$.menuItems.classList.toggle("hidden");
+    });
+  },
+};
+
+// this won't work just as menu.etc... because it's now App.$.menu
+// same for menuItems, which is now App.$.menuItems
+// However, like this we're letting things in the global scope; so we gotta add an init() method whitin the App namespace
+
+// menu.addEventListener("click", (event) => {
+//   menuItems.classList.toggle("hidden");
+// });
