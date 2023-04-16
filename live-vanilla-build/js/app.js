59748c4a74ff8dfa44ec77c4c9c7f445c7e66398
EvelineGomes <gomes.evepg@gmail.com>
2023-04-16 19:13:23 -0400

    initial ideia on adding click event to gameboard

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -1,12 +1,16 @@
 const App = {
+  // Selectors:
   $: {
     menu: document.querySelector('[data-id="menu"]'),
     menuItems: document.querySelector('[data-id="menu-items"]'),
     resetBtn: document.querySelector('[data-id="reset-btn"]'),
     newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id="square"]'), // "All" because we want all the elements to have the data-id of square
   },
 
   init() {
+    console.log(App.$.squares); // this shows in the browser console a nodelist with all squares. They have indexes so it'll be better to add to each HTML elements an id
+
     App.$.menu.addEventListener("click", (event) => {
       App.$.menuItems.classList.toggle("hidden");
     });
