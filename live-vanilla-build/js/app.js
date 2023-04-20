8c23f2fdd0d7f45c48cbaff5d5cda2c2a98498a6
EvelineGomes <gomes.evepg@gmail.com>
2023-04-20 14:06:47 -0400

    adding moves state

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -12,7 +12,7 @@ const App = {
     currentPlayer: 1,
     // track an array of game moves to finally start working on the winning possibilities
     // these moves will be from the player that has played
-    moves: [{ squareId: 1, player: 1 }], // example: this tracks if the player 1 clicked on square 1
+    moves: [], // leaving empty since it's the default
   },
 
   init() {
@@ -49,7 +49,18 @@ const App = {
           icon.classList.add("fa-solid", "fa-o", "turquoise");
         }
 
-        App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+        // As well as updating the state, we also want to update the move. This has to be before we update the current player:
+        App.state.moves.push({
+          squareId: +square.id, // we'll reference the square; the + sign will work as a coersion to make it a number type
+          playerId: currentPlayer, //
+        });
+        // the new move is going to be an object
+
+        // refactoring App.state.currentPlayer to currentPlayer since we already have this declared as const before
+        App.state.currentPlayer = currentPlayer === 1 ? 2 : 1;
+
+        // only to track purpose while implementing the new updates
+        console.log(App.state);
 
         square.replaceChildren(icon);
 
