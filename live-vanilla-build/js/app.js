5237f0285eff22f1bae6b2919b97fe2c851567ae
EvelineGomes <gomes.evepg@gmail.com>
2023-04-20 17:42:14 -0400

    comments

--- live-vanilla-build/js/app.js
+++ live-vanilla-build/js/app.js
@@ -32,18 +32,16 @@ const App = {
 
     App.$.squares.forEach((square) => {
       square.addEventListener("click", (event) => {
-        // Check if there's already a play, if so, return early
-        // Now, instead of checking whether the squares have child nodes (which actually relies on the DOM), we should check the state of the game!
-        // we use a help method which we'll name hasMove, where we define a function that takes a squareId (a number), and we look for this squareId in the array of moves
+        // Check if there's already a play, if so, return early:
+        // So 1st, check the state of the game!
         const hasMove = (squareId) => {
           const existingMove = App.state.moves.find(
             (move) => move.squareId === squareId
           );
-          return existingMove !== undefined; // it's the same as !== null
+          return existingMove !== undefined;
         };
-
+        // Then check if there's a move in that square already!
         if (hasMove(+squareId)) {
-          // use + to make sure it's a number!
           return;
         }
 
