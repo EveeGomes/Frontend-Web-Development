333aa41f8dd171cdc0858c9db2b6c7ae59f81735
EvelineGomes <gomes.evepg@gmail.com>
2023-04-26 21:12:07 -0400

    removing and adding some comments

--- live-vanilla-build/js/view.js
+++ live-vanilla-build/js/view.js
@@ -2,16 +2,18 @@ export default class View {
   $ = {};
 
   constructor() {
-    this.$.menu = document.querySelector('[data-id="menu"]');
-    this.$.menuBtn = document.querySelector('[data-id="menu-btn"]');
-    this.$.menuItems = document.querySelector('[data-id="menu-items"]');
-    this.$.resetBtn = document.querySelector('[data-id="reset-btn"]');
-    this.$.newRoundBtn = document.querySelector('[data-id="new-round-btn"]');
+    // ctrl + D: to use multi word edit of same words in Windows and Linux.
+    // ctrl + f: to use replace
+    this.$.menu = this.qs('[data-id="menu"]');
+    this.$.menuBtn = this.qs('[data-id="menu-btn"]');
+    this.$.menuItems = this.qs('[data-id="menu-items"]');
+    this.$.resetBtn = this.qs('[data-id="reset-btn"]');
+    this.$.newRoundBtn = this.qs('[data-id="new-round-btn"]');
     this.$.squares = document.querySelectorAll('[data-id="square"]');
-    this.$.modal = document.querySelector('[data-id="modal"]');
-    this.$.modalText = document.querySelector('[data-id="modal-text"]');
-    this.$.modalBt = document.querySelector('[data-id="modal-btn"]');
-    this.$.turn = document.querySelector('[data-id="turn"]');
+    this.$.modal = this.qs('[data-id="modal"]');
+    this.$.modalText = this.qs('[data-id="modal-text"]');
+    this.$.modalBt = this.qs('[data-id="modal-btn"]');
+    this.$.turn = this.qs('[data-id="turn"]');
 
     // UI-only event listeners:
     this.$.menuBtn.addEventListener("click", (event) => {
@@ -51,14 +53,9 @@ export default class View {
 
   // adding another method (utility method) to decrease the amount of querySelectors in the constuctor (check video: ~3h33min)
   qs(selector) {
-    // qs as query selector // selector in the argument will be a string
     const el = document.querySelector(selector);
-
-    // if we can't find an element, we throw an error
     if (!el) throw new Error("Could not find elements");
 
     return el;
-
-    // this helper method really helps the developer to "guarantee" we're in fact selecting an element
   }
 }
