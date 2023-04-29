ae83cccb3f88974aa86df72a5dd1e2e3c5547eaa
EvelineGomes <gomes.evepg@gmail.com>
2023-04-29 14:42:39 -0400

    starting the store module

---
+++ live-vanilla-build/js/store.js
@@ -0,0 +1,11 @@
+export default class Store {
+  contructor() {}
+
+  #getState() {}
+
+  #saveState() {}
+
+  // some documentations about state management:
+  // - redux (check best practice in their website);
+  // nowadays React is the most used one!
+}
