2115842f9e2fbafc2ff8eaedf1b6801ba160e018
EvelineGomes <gomes.evepg@gmail.com>
2023-04-30 16:46:56 -0400

    organizing get game()

--- live-vanilla-build/js/store.js
+++ live-vanilla-build/js/store.js
@@ -11,12 +11,16 @@ export default class Store {
 
   // The goal of this method is to take the raw state object (the array of game moves) and calculate useful information from that (like we did in getGameStatus() from App namespace)
   get game() {
-    // get the current player
+    // get the current player (4h:15min)
     const state = this.#getState();
 
-    // using modulus operator to determine the current player:
-    const currentPlayer = this.players[state.moves.length % 2]; //state.moves.length will let us know how many moves are in this current game. Then we use modulus of 2 which will gives either 1 or 0 based on the number of moves
-    // **** open the terminal and type node to use node js for testing stuff: check video 4h:17min (remember: this game always start with player 1)
+    // using modulus operator to derive the current player:
+    const currentPlayer = this.players[state.moves.length % 2];
+
+    // like this we return currentPlayer as part of an object
+    return {
+      currentPlayer,
+    };
   }
 
   #getState() {
