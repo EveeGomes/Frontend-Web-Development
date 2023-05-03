286cebe7ee3bd2c1a49c471f8ba67a9d61d4f859
EvelineGomes <gomes.evepg@gmail.com>
2023-05-03 09:37:27 -0400

    adding updateScoreBoard method

--- live-vanilla-build/js/view.js
+++ live-vanilla-build/js/view.js
@@ -12,6 +12,10 @@ export default class View {
     this.$.modalText = this.#qs('[data-id="modal-text"]');
     this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
     this.$.turn = this.#qs('[data-id="turn"]');
+    // select the elements
+    this.$.p1Wins = this.#qs('[data-id="p1-wins"]');
+    this.$.p2Wins = this.#qs('[data-id="p2-wins"]');
+    this.$.ties = this.#qs('[data-id="ties"]');
 
     this.$$.squares = this.#qsAll('[data-id="square"]');
 
@@ -43,8 +47,11 @@ export default class View {
   /**
    * DOM helper methods
    */
-
-  updateScoreBoard() {}
+  updateScoreBoard(p1Wins, p2Wins, ties) {
+    this.$.p1Wins.innerText = `${p1Wins} wins`;
+    this.$.p2Wins.innerText = `${p2Wins} wins`;
+    this.$.ties.innerText = `${ties} ties`;
+  }
 
   openModal(message) {
     this.$.modal.classList.remove("hidden");
